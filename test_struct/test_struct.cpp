b8341ad4d0959787d8e17e73408a43c67deccecd
CappuccinoCup <969837250@qq.com>
2021-12-21 14:48:06 +0800

    Add a struct type test case

--- test_struct/test_struct.cpp
+++ test_struct/test_struct.cpp
@@ -3,6 +3,7 @@
 extern "C" {
 	  int isTriangle(struct Triangle*);
       double foo(int, struct TwoTriangles*);
+      double bar(int, struct ThreeTriangles*);
 }
 
 struct Triangle {
@@ -15,6 +16,12 @@ struct TwoTriangles {
     struct Triangle a;
     int b;
     struct Triangle c;
+};
+
+struct ThreeTriangles {
+    struct Triangle a;
+    int b;
+    struct TwoTriangles c;
 };
 
 int main() {
@@ -35,4 +42,13 @@ int main() {
     std::cout << "foo(1, tt1) is: " << foo(1, &tt1) << std::endl;
     std::cout << "foo(2, tt2) is: " << foo(2, &tt2) << std::endl;
     std::cout << "foo(3, tt3) is: " << foo(3, &tt3) << std::endl;
+
+    struct ThreeTriangles ttt1, ttt2, ttt3;
+    ttt1.a = t1, ttt1.b = 1, ttt1.c = tt2;
+    ttt2.a = t2, ttt2.b = 2, ttt2.c = tt3;
+    ttt3.a = t3, ttt3.b = 3, ttt3.c = tt1;
+
+    std::cout << "bar(1, ttt1) is " << bar(1, &ttt1) << std::endl;
+    std::cout << "bar(2, ttt2) is " << bar(2, &ttt2) << std::endl;
+    std::cout << "bar(3, ttt3) is " << bar(3, &ttt3) << std::endl;
 }
